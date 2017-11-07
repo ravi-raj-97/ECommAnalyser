d583dd167a70afb971d62eb5e5c9c8aeb8e04b11
Ravi Raj <raviraj97@gmail.com>
2017-11-07 17:57:02 +0530

    HTML files

--- database_setup.py
+++ database_setup.py
@@ -18,26 +18,18 @@ class Categories(DecBase):
 	c_id = Column(String(5), primary_key=True)
 	cat_name = Column(String(25), nullable=False)
 
-class Subcategories(DecBase):
-
-	__tablename__='subcategories'
-
-	sc_id = Column(String(5),primary_key=True)
-	subcat_name = Column(String(25),nullable=False)
-
 class Items(DecBase):
 
 	__tablename__='items'
 
-	item_id = Column(String(5), primary_key=True)
+	item_id = Column(String(6), primary_key=True)
 	item_name = Column(String(25), nullable=False)
 	s_id = Column(String(5), ForeignKey('sites.s_id'), primary_key=True)
 	c_id = Column(String(5), ForeignKey('categories.c_id'), primary_key=True)
-	sc_id = Column(String(5), ForeignKey('subcategories.sc_id'), primary_key=True)
+	price = Column(Integer)
 	no_viewed = Column(Integer)
 	sites = relationship(Sites)
 	categories = relationship(Categories)
-	subcategories = relationship(Subcategories)
 
 class Sales(DecBase):
 
@@ -45,14 +37,12 @@ class Sales(DecBase):
 
 	s_id = Column(String(5), ForeignKey('sites.s_id'), primary_key=True)
 	c_id = Column(String(5), ForeignKey('categories.c_id'), primary_key=True)
-	sc_id = Column(String(5), ForeignKey('subcategories.sc_id'), primary_key=True)
-	item_id = Column(String(5), ForeignKey('items.item_id'), primary_key=True)
-	price = Column(Integer)
+	item_id = Column(String(6), ForeignKey('items.item_id'), primary_key=True)
+	month = Column(String(10), primary_key=True)
 	no_sold = Column(Integer)
 	sites = relationship(Sites)
 	categories = relationship(Categories)
 	items = relationship(Items)
-	subcategories = relationship(Subcategories)
 
 	 
 class Rating(DecBase):
@@ -61,13 +51,12 @@ class Rating(DecBase):
 
 	s_id = Column(String(5),  ForeignKey('sites.s_id'), primary_key=True)
 	c_id = Column(String(5),  ForeignKey('categories.c_id'), primary_key=True)
-	sc_id = Column(String(5), ForeignKey('subcategories.sc_id'), primary_key=True)
-	item_id = Column(String(5),  ForeignKey('items.item_id'), primary_key=True)
+	item_id = Column(String(6),  ForeignKey('items.item_id'), primary_key=True)
+	month = Column(String(10), primary_key=True)
 	no_stars = Column(Integer)
 	sites = relationship(Sites)
 	categories = relationship(Categories)
 	items = relationship(Items)
-	subcategories = relationship(Subcategories)
 
 
 engine = create_engine(
